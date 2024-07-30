a8def526e4155524b4903c6df89ce1a158c79ef9
Mouadh Ounissi <ounissimouadh45@gmail.com>
2024-07-30 12:18:56 +0100

    Add unit tests for MemberController

--- src/test/java/com/ounitech/wemove/controllers/MemberControllerTest.java
+++ src/test/java/com/ounitech/wemove/controllers/MemberControllerTest.java
@@ -4,20 +4,29 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ounitech.wemove.controllers.MemberController;
 import com.ounitech.wemove.models.Member;
 import com.ounitech.wemove.services.MemberService;
+import com.ounitech.wemove.services.MemberSubscriptionService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
 
 @WebMvcTest(MemberController.class)
 class MemberControllerTest {
@@ -27,6 +36,65 @@ class MemberControllerTest {
 
     @MockBean
     private MemberService memberService;
+
+    @MockBean
+    private MemberSubscriptionService memberSubscriptionService;
+
+    @Test
+    void saveMemberTest() throws Exception {
+        Member member = new Member();
+        member.setId(1000);
+        member.setFirstname("saad");
+        member.setLastname("bguir");
+        member.setEmail("saad@gmail.com");
+
+        Mockito.when(memberService.findByEmail(any(String.class))).thenReturn(null);
+        Mockito.when(memberService.save(member)).thenReturn(member);
+
+        MockHttpServletResponse response = mockMvc.perform(
+                MockMvcRequestBuilders.post("/api/members/save").contentType(MediaType.APPLICATION_JSON).content(asJsonString(member))).andReturn().getResponse();
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.CREATED.value());
+        assertThat(response.getContentAsString().contains("saad")).isTrue();
+        assertThat(response.getContentAsString().contains("bguir")).isTrue();
+        assertThat(response.getContentAsString().contains("saad@gmail.com")).isTrue();
+    }
+
+    @Test
+    void saveMemberTest_DuplicateEmail() throws Exception {
+        Member member = new Member();
+        member.setId(1000);
+        member.setFirstname("saad");
+        member.setLastname("bguir");
+        member.setEmail("saad@gmail.com");
+
+        Member member2 = new Member();
+        member2.setEmail("saad@gmail.com");
+
+        Mockito.when(memberService.findByEmail(any(String.class))).thenReturn(member2);
+
+        MockHttpServletResponse response = mockMvc.perform(
+                MockMvcRequestBuilders.post("/api/members/save").contentType(MediaType.APPLICATION_JSON).content(asJsonString(member))).andReturn().getResponse();
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.CONFLICT.value());
+    }
+
+    @Test
+    void saveMemberTest_missing_fields() throws Exception {
+        //Given
+        Member member = new Member();
+        member.setId(1000);
+        member.setEmail("saad@gmail.com");
+
+        Mockito.when(memberService.findByEmail(any(String.class))).thenReturn(null);
+
+        // When
+        MockHttpServletResponse response = mockMvc.perform(
+                MockMvcRequestBuilders.post("/api/members/save").contentType(MediaType.APPLICATION_JSON).content(asJsonString(member))).andReturn().getResponse();
+
+        // Then
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
 
     @Test
     void findById_nominal_case() throws Exception {
@@ -38,7 +106,7 @@ class MemberControllerTest {
         Mockito.when(memberService.findById(7)).thenReturn(Optional.of(member));
 
         // When
-        MockHttpServletResponse response = mockMvc.perform(MockMvcRequestBuilders.get("/api/members/7")).andReturn().getResponse();
+        MockHttpServletResponse response = mockMvc.perform(MockMvcRequestBuilders.get("/api/members/findById/7")).andReturn().getResponse();
 
         // Then
         assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
@@ -50,7 +118,7 @@ class MemberControllerTest {
     void findById_not_found() throws Exception {
         // When
         Mockito.when(memberService.findById(7)).thenReturn(Optional.empty());
-        MockHttpServletResponse response = mockMvc.perform(MockMvcRequestBuilders.get("/api/members/7")).andReturn().getResponse();
+        MockHttpServletResponse response = mockMvc.perform(MockMvcRequestBuilders.get("/api/members/findById/7")).andReturn().getResponse();
 
         // Then
         assertThat(response.getStatus()).isEqualTo(HttpStatus.NOT_FOUND.value());
@@ -84,13 +152,12 @@ class MemberControllerTest {
         member1.setLastname("aaaaa");
         member1.setEmail("aaaaa");
 
-        memberService.save(member1);
 
-        Mockito.when(memberService.findById(4)).thenReturn(Optional.of(member1));
-        Mockito.when(memberService.updateMember(Mockito.any(Integer.class), Mockito.any(Member.class))).thenReturn(member);
+        Mockito.when(memberService.findById(any(Integer.class))).thenReturn(Optional.of(member1));
+        Mockito.when(memberService.updateMember(any(Integer.class), any(Member.class))).thenReturn(member);
 
         MockHttpServletResponse response = mockMvc.perform(
-                MockMvcRequestBuilders.put("/api/members/4").contentType(MediaType.APPLICATION_JSON).content(asJsonString(member))).andReturn().getResponse();
+                MockMvcRequestBuilders.put("/api/members/update/4").contentType(MediaType.APPLICATION_JSON).content(asJsonString(member))).andReturn().getResponse();
 
         assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
         assertThat(response.getContentAsString().contains("phil")).isTrue();
@@ -104,4 +171,79 @@ class MemberControllerTest {
             throw new RuntimeException(e);
         }
     }
+
+    @Test
+    void activateUserTest() throws Exception {
+        Member member = new Member();
+        member.setFirstname("phil");
+        member.setLastname("webb");
+        member.setEmail("webb@gmail.com");
+        member.setActive((byte) 0);
+        member.setId(1000);
+
+        Member activatedMember = new Member();
+        member.setFirstname("phil");
+        member.setLastname("webb");
+        member.setEmail("webb@gmail.com");
+        member.setActive((byte) 1);
+        member.setId(1000);
+
+        Mockito.when(memberService.findById(any(Integer.class))).thenReturn(Optional.of(member));
+        Mockito.when(memberService.activateMember(1000)).thenReturn(activatedMember);
+
+        MockHttpServletResponse response = mockMvc.perform(MockMvcRequestBuilders.put("/api/members/activate/1000"))
+                .andReturn().getResponse();
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+    }
+
+    @Test
+    void findMembersWithPagination_nominal_case() throws Exception {
+        // Given
+        Member member1 = new Member();
+        member1.setFirstname("phil");
+        member1.setLastname("webb");
+        member1.setId(1000);
+
+        Member member2 = new Member();
+        member2.setFirstname("john");
+        member2.setLastname("doe");
+        member2.setId(1001);
+
+        List<Member> members = Arrays.asList(member1, member2);
+        Page<Member> memberPage = new PageImpl<>(members);
+
+        Mockito.when(memberService.findMembersWithPagination(anyInt())).thenReturn(memberPage);
+
+        // When
+        MockHttpServletResponse response = mockMvc.perform(MockMvcRequestBuilders.get("/api/members/page/1"))
+                .andReturn().getResponse();
+
+        // Then
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.getContentAsString()).contains("phil");
+        assertThat(response.getContentAsString()).contains("webb");
+        assertThat(response.getContentAsString()).contains("john");
+        assertThat(response.getContentAsString()).contains("doe");
+    }
+
+    @Test
+    void findMembersWithPagination_empty_page() throws Exception {
+        // Given
+        Page<Member> emptyPage = Page.empty();
+
+        Mockito.when(memberService.findMembersWithPagination(anyInt())).thenReturn(emptyPage);
+
+        // When
+        MockHttpServletResponse response = mockMvc.perform(MockMvcRequestBuilders.get("/api/members/page/1"))
+                .andReturn().getResponse();
+
+        // Then
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.getContentAsString()).doesNotContain("phil");
+        assertThat(response.getContentAsString()).doesNotContain("webb");
+        assertThat(response.getContentAsString()).doesNotContain("john");
+        assertThat(response.getContentAsString()).doesNotContain("doe");
+    }
 }
+
