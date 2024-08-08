a5a1a736b9d20a69fade14e28e2c174157ae911b
mejdi <mejdi@ounitech.net>
2024-08-08 12:01:19 +0200

    repository : add tests

--- src/test/java/com/ounitech/wemove/repositories/EntryRepositoryTest.java
+++ src/test/java/com/ounitech/wemove/repositories/EntryRepositoryTest.java
@@ -1,22 +1,53 @@
 package com.ounitech.wemove.repositories;
 
-import org.junit.jupiter.api.AfterEach;
+import com.ounitech.wemove.models.Entry;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+import java.util.function.Function;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
 class EntryRepositoryTest {
+
+    @Autowired
+    private EntryRepository entryRepository;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     @BeforeEach
     void setUp() {
-    }
+        jdbcTemplate.execute("""
+                    INSERT INTO member(id, firstname, lastname, email, active) 
+                    VALUES (100, 'firstname', 'lastname', 'email', 1);
+                """);
 
-    @AfterEach
-    void tearDown() {
+        jdbcTemplate.execute("""
+                        INSERT INTO entry(id, memberid, entrytime, leavetime) 
+                        VALUES (1, 100, now(), now()); 
+                """);
+
+        jdbcTemplate.execute("""
+                        INSERT INTO entry(id, memberid, entrytime, leavetime) 
+                        VALUES (2, 100, now(), now()); 
+                """);
     }
 
     @Test
     void findByMemberId() {
+        // Given // When
+        List<Entry> entries = entryRepository.findByMemberId(100);
+
+        // Then
+        assertThat(entries)
+                .hasSize(2)
+                .extracting((Function<Entry, Integer>) Entry::getId)
+                .containsExactlyInAnyOrder(1, 2);
     }
 }
--- src/test/java/com/ounitech/wemove/repositories/StaffRepositoryTest.java
+++ src/test/java/com/ounitech/wemove/repositories/StaffRepositoryTest.java
@@ -1,30 +1,103 @@
 package com.ounitech.wemove.repositories;
 
-import org.junit.jupiter.api.AfterEach;
+import com.ounitech.wemove.models.Role;
+import com.ounitech.wemove.models.Staff;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
 class StaffRepositoryTest {
+
+    @Autowired
+    private StaffRepository staffRepository;
+
+    @Autowired
+    private TestEntityManager testEntityManager;
 
     @BeforeEach
     void setUp() {
-    }
+        Role managerRole = new Role();
+        managerRole.setRoleName("Manager");
 
-    @AfterEach
-    void tearDown() {
+        Role trainerRole = new Role();
+        trainerRole.setRoleName("Trainer");
+
+        testEntityManager.persist(managerRole);
+        testEntityManager.persist(trainerRole);
+
+        Staff staffOne = new Staff();
+        staffOne.setFirstname("kevin");
+        staffOne.setLastname("jones");
+        staffOne.setActive(true);
+        staffOne.setAddress("3 london street");
+        staffOne.setPhone("31 48 53 65");
+        staffOne.setPicture("https://dev.wemove.com/kevin.jones.jpg");
+        staffOne.setEmail("kevin.jones@gmail.com");
+        staffOne.setRole(managerRole);
+
+        Staff staffTwo = new Staff();
+        staffTwo.setFirstname("leila");
+        staffTwo.setLastname("robinson");
+        staffTwo.setActive(true);
+        staffTwo.setAddress("5 mandela avenue");
+        staffTwo.setPhone("51 24 85 62");
+        staffTwo.setPicture("https://dev.wemove.com/leila.robinson.jpg");
+        staffTwo.setEmail("leila.robinson@gmail.com");
+        staffTwo.setRole(trainerRole);
+
+        Staff staffThree = new Staff();
+        staffThree.setFirstname("adam");
+        staffThree.setLastname("wood");
+        staffThree.setActive(false);
+        staffThree.setAddress("7 palm springs corner");
+        staffThree.setPhone("71 12 53 68");
+        staffThree.setPicture("https://dev.wemove.com/adam.wood.jpg");
+        staffThree.setEmail("adam.wood@gmail.com");
+        staffThree.setRole(trainerRole);
+
+        staffRepository.saveAll(List.of(staffOne, staffTwo, staffThree));
     }
 
     @Test
     void findByfirstname() {
+        // Given // When
+        List<Staff> results = staffRepository.findByfirstname("kevin");
+
+        // Then
+        assertThat(results)
+                .hasSize(1)
+                .extracting(Staff::getLastname)
+                .containsExactly("jones");
     }
 
     @Test
     void findByactive() {
+        // Given // When
+        List<Staff> results = staffRepository.findByactive(true);
+
+        // Then
+        assertThat(results)
+                .hasSize(2)
+                .extracting(Staff::getLastname)
+                .containsExactlyInAnyOrder("jones", "robinson");
     }
 
     @Test
     void findByemail() {
+        // Given // When
+        Staff result = staffRepository.findByemail("leila.robinson@gmail.com");
+
+        // Then
+        assertThat(result)
+                .isNotNull()
+                .extracting(Staff::getLastname)
+                .isEqualTo("robinson");
     }
 }
