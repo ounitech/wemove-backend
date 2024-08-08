1dfee1ceb50026d366a6c59d5273db7b95c1544d
mejdi <mejdi@ounitech.net>
2024-08-08 07:01:07 +0200

    db : delete obsolete files

--- src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO users (firstname, lastname, email, job) VALUES ('mohamed', 'mohamed', 'mohamed@example.com', 'Developer');
-INSERT INTO users (firstname, lastname, email, job) VALUES ('ahmed', 'ahmed', 'mouadh@example.com', 'Designer');
-INSERT INTO users (firstname, lastname, email, job) VALUES ('mouadh', 'mouadh', 'mouadh@example.com', 'Designer');
