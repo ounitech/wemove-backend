3c2fd46130c377f11251f9335ea4eb79f8118804
mejdi <mejdi@ounitech.net>
2024-08-10 20:14:50 +0200

    todo : complete tests for StatsService and SubscriptionService

--- src/test/java/com/ounitech/wemove/services/StatsServiceTest.java
+++ src/test/java/com/ounitech/wemove/services/StatsServiceTest.java
@@ -1,38 +1,82 @@
 package com.ounitech.wemove.services;
 
+import com.ounitech.wemove.repositories.MemberRepository;
+import com.ounitech.wemove.repositories.MemberSubscriptionRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 class StatsServiceTest {
 
+    @Mock
+    private MemberRepository memberRepository;
+
+    @Mock
+    private MemberSubscriptionRepository memberSubscriptionRepository;
+
+    @InjectMocks
+    private StatsService statsService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
     @Test
     void getMembersCount() {
+        // Given
+        // When
+        // Then
     }
 
     @Test
     void getActiveMembersCount() {
+        // Given
+        // When
+        // Then
     }
 
     @Test
     void getInactiveMembersCount() {
+        // Given
+        // When
+        // Then
     }
 
     @Test
     void getGoldMembersCount() {
+        // Given
+        // When
+        // Then
     }
 
     @Test
     void getSilverMembersCount() {
+        // Given
+        // When
+        // Then
     }
 
     @Test
     void getBronzeMembersCount() {
+        // Given
+        // When
+        // Then
     }
 
     @Test
     void getMaleMembersCount() {
+        // Given
+        // When
+        // Then
     }
 
     @Test
     void getFemaleMembersCount() {
+        // Given
+        // When
+        // Then
     }
 }
--- src/test/java/com/ounitech/wemove/services/SubscriptionServiceTest.java
+++ src/test/java/com/ounitech/wemove/services/SubscriptionServiceTest.java
@@ -1,18 +1,43 @@
 package com.ounitech.wemove.services;
 
+import com.ounitech.wemove.repositories.SubscriptionRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 class SubscriptionServiceTest {
 
+    @Mock
+    private SubscriptionRepository subscriptionRepository;
+
+    @InjectMocks
+    private SubscriptionService subscriptionService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
     @Test
     void save() {
+        // Given
+        // When
+        // Then
     }
 
     @Test
     void findById() {
+        // Given
+        // When
+        // Then
     }
 
     @Test
     void findAll() {
+        // Given
+        // When
+        // Then
     }
 }
