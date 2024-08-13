403ddaea5349f21515368e2477503e4495d77938
mejdi <mejdi@ounitech.net>
2024-08-13 11:59:36 +0200

    StaffController : test failure

--- src/test/java/com/ounitech/wemove/controllers/StaffControllerTest.java
+++ src/test/java/com/ounitech/wemove/controllers/StaffControllerTest.java
@@ -1,7 +1,37 @@
 package com.ounitech.wemove.controllers;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.ounitech.wemove.services.StaffService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@WebMvcTest(StaffController.class)
 class StaffControllerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private StaffService staffService;
+
+    @Test
+    void findAllEntries() throws Exception {
+        // Given
+        // When
+        MockHttpServletResponse response = mockMvc.perform(
+                get("/api/entries/findAll")
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andReturn().getResponse();
+
+        // Then
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+    }
 }
