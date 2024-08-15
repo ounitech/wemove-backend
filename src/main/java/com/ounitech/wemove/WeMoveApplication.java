e9f42191b6878537017b98cf2cc1431c539ca4b7
Mouadh <ounissimouadh45@gmail.com>
2024-08-15 15:26:13 +0100

    remove unnecessary code

--- src/main/java/com/ounitech/wemove/WeMoveApplication.java
+++ src/main/java/com/ounitech/wemove/WeMoveApplication.java
@@ -6,6 +6,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+//@EnableAspectJAutoProxy
 public class WeMoveApplication {
     public static void main(String[] args) {
         SpringApplication.run(WeMoveApplication.class, args);
