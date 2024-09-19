663a0ea6bbdd49e5d4fcda5f9351d547a13a1102
Mouadh <ounissimouadh45@gmail.com>
2024-09-19 22:24:12 +0100

    add javadocs to EntryService

--- src/main/java/com/ounitech/wemove/services/EntryService.java
+++ src/main/java/com/ounitech/wemove/services/EntryService.java
@@ -7,23 +7,49 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+/**
+ * This class represents a service for handling member entries related operations.
+ * It includes methods to add,  retrieve member entries
+ */
 @Service
 public class EntryService {
 
     private final EntryRepository entryRepository;
 
+    /**
+     * Instantiates a new Entry service.
+     *
+     * @param entryRepository the entry repository
+     */
     EntryService(EntryRepository entryRepository) {
         this.entryRepository = entryRepository;
     }
 
+    /**
+     * Get all entries from the repository.
+     *
+     * @return the list of all entries
+     */
     public List<Entry> findAllEntries() {
         return entryRepository.findAll();
     }
 
+    /**
+     * Create a new entry.
+     *
+     * @param entry the entry object to be saved
+     * @return the saved entry
+     */
     public Entry createEntry(Entry entry) {
         return entryRepository.save(entry);
     }
 
+    /**
+     * Find the member's entries by id.
+     *
+     * @param memberId the ID of member to find
+     * @return the list of member's entries
+     */
     public List<Entry> findEntriesByMember(Integer memberId) {
         return entryRepository.findByMemberId(memberId);
     }
